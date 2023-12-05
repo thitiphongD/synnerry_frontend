70fb0f774c1b0834a7d438a60f353d93ce306f5e
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2023-12-05 19:54:38 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,54 @@
 <template>
-  <section
-    class="text-gray-600 body-font h-screen flex bg-gray-900 bg-svg-constellation-gray-100 relative"
-  >
-    <div
-      class="container mx-auto flex px-5 py-12 items-center justify-center flex-col"
-    >
-      <div class="lg:w-2/3 w-full animate-fade-in-down">
-        <h1
-          class="md:text-6xl text-3xl mb-2 font-bold text-white tracking-tight leading-tight"
-        >
-          Hello, Synerry Corporation.
-        </h1>
-        <h1
-          class="md:text-6xl text-3xl mb-4 font-bold text-white tracking-tight leading-tight"
-        >
-          My name is Thitiphong Srisavat.
-        </h1>
-        <h1
-          class="md:text-6xl text-3xl mb-4 font-bold text-white tracking-tight leading-tight"
-        >
-          I'm here to assist Project.
-        </h1>
-        <p
-          class="md:leading-relaxed leading-normal text-white tracking-tight text-xl"
-        >
-          Deep fried eggs are an ingredient in the Indonesian dish balado tahu
-          telor, which consists of deep fried eggs, deep fried tofu and chili
-          peppers. Kwek-kwek is prepared using quail eggs The deep fried egg is
-          a popular dish in Asia.Tokneneng is a tempura-like Filipino street
-          food made by deep-frying annatto-dyed orange batter covered
-          hard-boiled chicken or duck eggs. Kwek-kwek is a Filipino street food
-          consisting of orange-battered and deep fried quail eggs. Deep fried
-          balut consists of a fertilized developing egg embryo that is boiled
-          with the shell on and then deep fried. It is sometimes served on a
-          stick in the Philippines.Son-in-law eggs (Thai: kai look keuy) is a
-          Thai dish prepared using deep fried hard boiled eggs and a sweet and
-          sour caramel sauce. It is a relatively common street food in Thailand.
-          In Thai folklore, some sources state that the dish is served as a
-          warning to a mother's son-in-law who is mistreating his wife, or as a
-          warning to a future son-in-law to not mistreat his wife, in which the
-          dish is served to the son-in-law, with the eggs symbolically
-          representing fried testicles.Prepared deviled eggs can be deep fried
-          and served as a hot dish, too.
-        </p>
-      </div>
-    </div>
-  </section>
+  <div>
+    <h1>
+      Visit Counter <span>{{ counter }}</span>
+    </h1>
+    <p>Device: {{ device }}</p>
+    <p>Browser: {{ browser }}</p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
 
-<style scoped></style>
+const counter = ref(0);
+const device = ref("");
+const browser = ref("");
+
+onMounted(() => {
+  // Retrieve counter value from localStorage
+  const storedCounter = localStorage.getItem("visitCounter");
+
+  // If a value exists in localStorage, use it; otherwise, initialize to 0
+  counter.value = storedCounter ? parseInt(storedCounter, 10) : 0;
+
+  // Increment the counter when the component is mounted (page is visited)
+  counter.value += 1;
+
+  // Save the updated counter value to localStorage
+  localStorage.setItem("visitCounter", counter.value.toString());
+
+  // Get device information
+  device.value = getDevice();
+
+  // Get browser information
+  browser.value = getBrowser();
+});
+
+// Function to get device information
+const getDevice = () => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  return isMobile ? "Mobile" : "Desktop";
+};
+
+// Function to get browser information
+const getBrowser = () => {
+  const userAgent = navigator.userAgent;
+  if (userAgent.includes("Chrome")) return "Google Chrome";
+  if (userAgent.includes("Safari")) return "Safari";
+  if (userAgent.includes("Firefox")) return "Mozilla Firefox";
+  if (userAgent.includes("Edge")) return "Microsoft Edge";
+  if (userAgent.includes("Opera") || userAgent.includes("OPR")) return "Opera";
+  return "Unknown";
+};
+</script>
